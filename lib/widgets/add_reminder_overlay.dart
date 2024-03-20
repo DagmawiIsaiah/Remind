1beb82ceba63d563c3f10bb4718b741070c4e80a
Dagmawi Isaiah <161498434+DagmawiIsaiah@users.noreply.github.com>
2024-03-20 18:58:10 +0300

    Create add reminder overlay widget

---
+++ lib/widgets/add_reminder_overlay.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddReminderOverlay extends StatelessWidget {
+  const AddReminderOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
